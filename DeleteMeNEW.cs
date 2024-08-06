229bffc05b1af856905ee8761fb41c87a5727a1e
sherlockgames <harshuajmani@gmail.com>
2024-08-07 04:54:02 +0530

    input system

---
+++ DeleteMeNEW.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class DeleteMeNEW : MonoBehaviour
+{
+    // New Input system cripot 
+    [SerializeField] InputAction movement;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        movement.Enable(); 
+    }
+    
+    void OnDisable()
+    {
+        movement.Disable(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalThrow = movement.ReadValue<Vector2>.x;
+        float verticalThrow = movement.ReadValue<Vector2>.y;
+
+        Debug.Log(horizontalThrow);
+        Debug.Log(verticalThrow);
+    }
+}
